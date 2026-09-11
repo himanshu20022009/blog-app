0607ca602e9fa40e9cffd48f2bcf82368132ba54
himanshu <himanshu.y995@gmail.com>
2026-09-11 09:55:19 +0530

    updated url

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BACKEND_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:4001";
+  import.meta.env.VITE_API_URL || "https://blog-app-sclm.onrender.com";
 
 axios.defaults.withCredentials = true;
 
